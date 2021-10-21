cfa9d30f7dbfdb42595693f78c4ac83717d273f1
Stoner Chavira <slchavira@gmail.com>
2021-10-21 01:57:49 -0500

    Uploading completed movie app.

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-// let movies = [];
-
-// let title = $("#movie-title").value();
-// console.log(title);
-
 $("#submit-button").on("click", function() {
     //assign movie title and rating from input boxes to variables.
     let movieTitle = $("#movie-title").val();
     let movieRating = $("#movie-rating").val();
     let movieElement = $("<li>" + movieTitle + "&emsp;" + movieRating + "&emsp;" + "<button id='delete'>X</button>" + "</li>");
     $("ul").append(movieElement);
+    //deletes Movie Title and Rating fields after submission
+    $("#movie-title").val('');  
+    $("#movie-rating").val('');
     return false; //prevents page reload
 });
 
-$("button #delete").on("click", function() {
-    console.log(this);
+$("#movie-ul").on("click", "#delete", function(){
+    $(this).parent().remove();
 });
